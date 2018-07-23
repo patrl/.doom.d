13b215a4fb508eb356fa7ebd07972b16aeff9bb8
Patrick Elliott <patrick.d.elliott@gmail.com>
2018-07-23 10:54:28 +0200

    Added useful packages

---
+++ packages.el
@@ -0,0 +1,8 @@
+;; -*- no-byte-compile: t; -*-
+;;; ~/.doom.d/packages.el
+
+(package! hlint-refactor :recipe (:fetcher github :repo "mpickering/hlint-refactor-mode"))
+
+(package! deadgrep)
+
+(package! olivetti)
