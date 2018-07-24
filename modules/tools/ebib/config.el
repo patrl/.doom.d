d56a232a2029853eed882618a47bb8958e092b1e
Patrick Elliott <patrick.d.elliott@gmail.com>
2018-07-24 13:54:09 +0200

    Started on ebib keymap

--- modules/tools/ebib/config.el
+++ modules/tools/ebib/config.el
@@ -1,7 +1,16 @@
 ;;; tools/ebib/config.el -*- lexical-binding: t; -*-
 
-(def-package! ebib
-  :commands ebib
-  :config
+(after! ebib
   (setq ebib-bibtex-dialect 'biblatex
-        ebib-preload-bib-files '("/home/patrl/GitHub/bibliography/elliott_mybib.bib")))
+        ebib-preload-bib-files '("/home/patrl/GitHub/bibliography/elliott_mybib.bib")
+
+        ebib-index-mode-map (make-sparse-keymap))
+  (map! (:map (ebib-index-mode-map)
+          :n "k" #'ebib-prev-entry
+          :n "j" #'ebib-next-entry
+          :n "l" #'ebib-next-database
+          :n "h" #'ebib-prev-database
+          :n "gg" #'ebib-goto-first-entry
+          :n "G" #'ebib-goto-last-entry
+          :n "q" #'ebib-quit)))
+
