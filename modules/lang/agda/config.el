378be0a210e59c63cd5a160c4ce7174d1de617c1
Patrick Elliott <patrick.d.elliott@gmail.com>
2018-08-25 17:34:18 +0200

    Updated agda-mode

--- modules/lang/agda/config.el
+++ modules/lang/agda/config.el
@@ -1,18 +1,20 @@
 ;;; lang/agda/config.el -*- lexical-binding: t; -*-
 
-(defvar +agda-dir (string-remove-suffix "/agda2.el" (shell-command-to-string "agda-mode locate")))
+;; (defvar +agda-dir (string-remove-suffix "/agda2.el" (shell-command-to-string "agda-mode locate")))
+(defvar +agda-dir
+  (when (executable-find "agda-mode")
+    (file-name-directory (shell-command-to-string "agda-mode locate"))))
 
 (def-package! agda-input
   :load-path +agda-dir)
 
-(def-package! agda2
-  :defer t)
-
-(after! agda2-mode
+(def-package! agda2-mode
+  :mode "\\.agda\\'"
+  :after agda-input
+  :config
   (map! :map agda2-mode-map
                 :localleader
                 :n "l" #'agda2-load
                 :n "d" #'agda2-infer-type-maybe-toplevel
                 :n "o" #'agda2-module-contents-maybe-toplevel
-                :n "n" #'agda2-compute-normalised-maybe-toplevel
-                ))
+                :n "n" #'agda2-compute-normalised-maybe-toplevel))
