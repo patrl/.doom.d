79b04b1de3a052b4f55a2c29130bb7dd0c177154
Patrick Elliott <patrick.d.elliott@gmail.com>
2018-09-03 13:22:16 +0200

    Update config

--- config.el
+++ config.el
@@ -5,30 +5,34 @@
 
 (load! "+bindings.el")
 
-(setq magit-process-find-password-functions '(magit-process-password-auth-source))
-
 (setq org-directory (expand-file-name "~/Sync/org/")
       org-agenda-files (list org-directory)
       org-ellipsis " ▼ "
+      org-highlight-latex-and-related '(latex)
       org-bullets-bullet-list '("#"))
 
 (after! deft
   (setq deft-directory "~/Sync/deft"))
 
-(setq nix-indent-function 'nix-indent-line)
+;; nix tweaks
+(after! nix-mode
+  (setq nix-indent-function 'nix-indent-line))
+
+(setq bibtex-completion-library-path "~/Sync/library/"
+      helm-bibtex-full-frame nil
+      bibtex-completion-pdf-symbol ""
+      +latex-bibtex-file "~/GitHub/bibliography/elliott_mybib.bib"
+      +latex-viewers `(pdf-tools zathura))
+
+;; (setq ivy-bibtex-default-action 'ivy-bibtex-open-pdf)
 
 (after! tex
   (setq-default TeX-engine 'xetex))
 
-(setq markdown-command "pandoc --filter pandoc-citeproc --standalone --css=http://benjam.info/pan-am/styling.css -V lang=en -V highlighting-css= --mathjax --from=markdown+smart --to=html5"
-      markdown-enable-math t)
+;; enable unicode-math completion:
+(setq +latex-enable-unicode-math t)
 
-(setq bibtex-completion-library-path "~/Sync/library/"
-      +latex-bibtex-file "~/GitHub/bibliography/elliott_mybib.bib"
-      +latex-enable-unicode-math t
-      +latex-viewers `(pdf-tools zathura)
-      ivy-bibtex-default-action 'ivy-bibtex-open-pdf)
-
+;; ensures that unicode symbol completion only happens in math-mode
 (after! latex
   (add-hook! 'LaTeX-mode-hook
       (setq-local company-math-allow-unicode-symbols-in-faces (quote (tex-math font-latex-math-face)))
@@ -36,18 +40,18 @@
 
 (setq doom-font (font-spec :family "Input Mono" :size 11.0)
       doom-variable-pitch-font (font-spec :family "Input Sans")
-      doom-unicode-font (font-spec :family "DejaVu Sans Mono")
+      doom-unicode-font (font-spec :name "DejaVu Sans Mono")
       doom-big-font (font-spec :family "Input Mono" :size 18.0)
-      doom-theme 'doom-one-light)
+      doom-theme 'doom-dracula)
 
+;; refactoring support in haskell mode
 (def-package! hlint-refactor
   :hook (haskell-mode . hlint-refactor-mode))
 
-(setq org-highlight-latex-and-related '(latex))
-
+;; handy keybinding for deadgrep
 (global-set-key (kbd "<f5>") #'deadgrep)
 
-;; emacs/eshell
+;; eshell aliases
 (after! eshell
   (set-eshell-alias!
    "f"   "find-file $1"
@@ -58,31 +62,34 @@
    "gc"  "magit-commit"
    "rg"  "rg --color=always $*"))
 
+;; batteries-included magit setup:
 (setq +magit-hub-features t
-      magithub-clone-default-directory "~/GitHub/"
-      magit-commit-arguments '("--gpg-sign=7CA109C3974AF5FA")
-      magit-rebase-arguments '("--autostash" "--gpg-sign=7CA109C3974AF5FA")
-      magit-pull-arguments '("--rebase" "--autostash" "--gpg-sign=7CA109C3974AF5FA"))
+      magit-process-find-password-functions '(magit-process-password-auth-source)
+      magithub-clone-default-directory "~/GitHub/")
 
-
-(after! magit
-  ;; Add gpg-sign to rebasing by default
-  (magit-define-popup-option 'magit-rebase-popup
-    ?S "Sign using gpg" "--gpg-sign=" #'magit-read-gpg-secret-key))
+;; (after! magit
+  ;; Add gpg-sign to rebasing by default (stolen from Henrik)
+  ;; (magit-define-popup-option 'magit-rebase-popup
+    ;; ?S "Sign using gpg" "--gpg-sign=" #'magit-read-gpg-secret-key)
 
 ;; disable line numbers in text and derived modes.
 (add-hook 'text-mode-hook #'doom|disable-line-numbers)
 
-;; tweaks for markdown mode.
-(add-hook 'markdown-mode-hook #'doom|disable-line-numbers)
-(add-hook 'markdown-mode-hook 'visual-line-mode)
-(add-hook 'markdown-mode-hook 'turn-on-olivetti-mode)
+(after! markdown-mode
+  ;; export markdown to beautiful html via pandoc
+  (setq markdown-command "pandoc --filter pandoc-citeproc --standalone --css=http://benjam.info/pan-am/styling.css -V lang=en -V highlighting-css= --mathjax --from=markdown+smart --to=html5"
+        ;; enable math highlighting
+        markdown-enable-math t)
+  ;; make markdown-mode less visually distracting:
+  (add-hook! markdown-mode '(visual-line-mode turn-on-olivetti-mode)))
 
+;; active dante-mode snippets in dante-mode (since it's a minor mode)
 (add-hook 'dante-mode-hook
           #'(lambda ()
               (yas-activate-extra-mode 'dante-mode)))
 
-;; `counsel-linux-app` looks in the right place for applications
-(setq counsel-linux-apps-directories '("/var/run/current-system/sw/share/applications"))
+;; counsel-linux-app looks in the right place for applications
+;; (setq counsel-linux-apps-directories '("/var/run/current-system/sw/share/applications"))
 
+;; avoid incompatibilities between tramp and zsh
 (eval-after-load 'tramp '(setenv "SHELL" "/bin/bash"))
