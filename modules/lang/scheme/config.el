ed332d36aaf6031c77d2056fde8eeeec889e30b5
Patrick Elliott <patrick.d.elliott@gmail.com>
2019-01-18 14:29:47 +0100

    Added scheme module.

---
+++ modules/lang/scheme/config.el
@@ -0,0 +1,4 @@
+;;; lang/scheme/config.el -*- lexical-binding: t; -*-
+
+;; (def-package! geiser
+;;  :ensure t)
---
+++ modules/lang/scheme/packages.el
@@ -0,0 +1,5 @@
+;; -*- no-byte-compile: t; -*-
+;;; lang/scheme/packages.el
+
+;;(package! quack :recipe (:fetcher url :url "https://www.neilvandyke.org/quack/quack.el"))
+;;(package! geiser)
