eae0882ee50dc874925e9d3762125a7daa1d9779
Patrick Elliott <patrick.d.elliott@gmail.com>
2020-01-26 23:30:50 -0500

    tweaks

--- packages.el
+++ packages.el
@@ -1,13 +1,11 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ~/.doom.d/packages.el
 
-;; (package! hlint-refactor :recipe (:fetcher github :repo "mpickering/hlint-refactor-mode"))
-
 (package! deadgrep)
 
 (package! org-brain)
 
-(package! ascii-art-to-unicode)
+(package! ascii-art-to-unicode) ;; to make org-brain beautiful
 
 (package! org-cliplink)
 
