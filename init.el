ee3b71328336d8bf2c78d70e93e86e5eede48762
Patrick Elliott <patrick.d.elliott@gmail.com>
2020-03-28 13:13:18 -0400

    Switch from lsp back to dante

--- init.el
+++ init.el
@@ -63,12 +63,13 @@
        data              ; config/data formats
        emacs-lisp        ; drown in parentheses
        javascript
-       (haskell +lsp)  ; a language that's lazier than I am
-       ;; (haskell +dante)  ; a language that's lazier than I am
+       ;; (haskell +lsp)  ; a language that's lazier than I am
+       (haskell +dante)  ; a language that's lazier than I am
        (latex +latexmk +pdf-tools +unicode-math)
        (markdown +pandoc)
        nix               ; I hereby declare "nix geht mehr!"
        (org +jupyter +pandoc +beamer)
+       purescript
        racket
        rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        (sh +fish)
